aaff760c45170b949161ed4b0c8d15833c264c76
Jos Goossen <jos.goossen@scheldestromen.nl>
2017-03-19 18:12:31 +0100

    Add files via upload

---
+++ druk_temp_rf_receiver.ino
@@ -0,0 +1,75 @@
+// druk_temp_rf_receiver
+// Jos Goossen
+// multiflexmeter.nl
+
+#include <DataCoder.h>
+#include <VirtualWire.h>
+#include "Wire.h" // For I2C
+#include "LCD.h" // For LCD
+#include "LiquidCrystal_I2C.h" // http://www.electroschematics.com/12459/arduino-i2c-lcd-backpack-introductory-tutorial/
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
+
+const int rx_pin = 11;
+const int led_pin = 13;
+const int baudRate = 1000;
+
+void setup()
+{
+  delay(1000);
+  Serial.begin(9600);
+  pinMode(led_pin,OUTPUT);
+  digitalWrite(led_pin,LOW);
+  SetupRFDataRxnLink(rx_pin, baudRate);
+
+  // Set off LCD module
+  lcd.begin (16,2); // 16 x 2 LCD module
+  lcd.setBacklightPin(3,POSITIVE); // BL, BL_POL
+  lcd.setBacklight(HIGH);
+}
+
+void loop()
+{
+   lcd.home (); // Set cursor to 0,0
+   lcd.print("Multiflexmeter"); // Custom text
+   lcd.setCursor (0,1); // Go to home of 2nd line
+  
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  union RFData inDataSeq;//To store incoming data
+  float inArray[RF_DATA_ARRAY_SIZE];
+  if(RFLinkDataAvailable(buf, &buflen))
+  {
+        digitalWrite(led_pin, HIGH);
+        for(int i =0; i< buflen; i++)
+        {
+          inDataSeq.s[i] = buf[i];
+        }
+        Serial.println();
+        digitalWrite(led_pin, LOW);
+        DecodeRFData(inArray, inDataSeq);
+
+        //Toon ontvangen Diepte en temp op display
+        int Diepte = inArray[0];
+        String druk = "Diepte: ";
+        druk += Diepte;
+        lcd.print(druk);
+        delay(2000); 
+        druk = "               ";
+        lcd.setCursor (0,1); // ga naar begin van 2e regel
+        lcd.print(druk);
+        Serial.print("Diepte:");
+        Serial.println(inArray[0]);
+ //       delay(1000);
+        int temp = inArray[1];
+        String temperatuur = "Temperatuur: ";
+        temperatuur += temp;
+        lcd.setCursor (0,1); // ga naar begin van 2e regel
+        lcd.print ("                ");
+        lcd.setCursor (0,1); // ga naar begin van 2e regel
+        lcd.print(temperatuur);
+        delay(2000);
+        lcd.setCursor (0,1); // ga naar begin van 2e regel
+        lcd.print ("                ");
+  }  
+}
+
---
+++ druk_temp_rf_transmitter.ino
@@ -0,0 +1,46 @@
+// druk_temp_rf_transmitter
+// Jos Goossen
+// multiflexmeter.nl
+
+#include <DataCoder.h>
+// https://bayesianadventures.wordpress.com/2013/09/20/arduino-rf-communications-data-encoder-for-virtualwire/
+#include <VirtualWire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+
+const int transmit_pin = 12;
+const int led_pin = 13;
+const int baudRate = 1000;
+
+void setup()
+{
+  pinMode(led_pin,OUTPUT);
+  SetupRFDataTxnLink(transmit_pin, baudRate);
+  sensors.begin();
+}
+
+void loop()
+{
+  delay(50);
+  sensors.requestTemperatures();
+  int sensorVoltage = analogRead(0);
+  // voor 30 psi sensor
+  int cmdiepte = 68.95*(sensorVoltage-125)/27.27;
+  float outArray[RF_DATA_ARRAY_SIZE];// aan te passen in  DataCoder.h
+  
+  outArray[0] = cmdiepte;
+  outArray[1] = sensors.getTempCByIndex(0);
+  
+  union RFData outDataSeq;
+  EncodeRFData(outArray, outDataSeq); 
+  TransmitRFData(outDataSeq);  
+  digitalWrite(led_pin, HIGH);
+  delay(100);
+  digitalWrite(led_pin,LOW);
+  delay(500);
+  }
